993c4f9838768ad3d18446532225a77fe68b3855
Lucas Lacerda <lucasalvlacerda@gmail.com>
2019-11-06 22:45:48 -0300

    :tada: initial commit

--- src/routes/cursoRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cursoController = require('../controllers/cursoController')
-
-router.route('/curso')
-    .get(cursoController.getAll)
-
-module.exports = router;
